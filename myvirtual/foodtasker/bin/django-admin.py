87d79e6a31df58bedba7694aeae70392dc52b665
toanhtran <toanh.t.tran@gmail.com>
2016-12-07 21:32:31 -0800

    add gitignore

---
+++ myvirtual/foodtasker/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/toanhtran/Desktop/iOS-Dev/Code4Startup/foodtasker/myvirtual/foodtasker/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
