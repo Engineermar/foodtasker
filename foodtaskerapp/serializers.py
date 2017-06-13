7167193d7cd81b174e3d914f855956f6ae607c20
toanhtran <toanh.t.tran@gmail.com>
2017-06-13 12:28:47 -0700

    fix logo link in api

--- foodtaskerapp/serializers.py
+++ foodtaskerapp/serializers.py
@@ -4,6 +4,13 @@ from foodtaskerapp.models import Restaurant
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
+    logo = serializers.SerializerMethodField()
+
+    def get_logo(self, restaurant):
+        request = self.context.get('request')
+        logo_url = restaurant.logo.url
+        return request.build_absolute_uri(logo_url)
+
     class Meta:
         model = Restaurant
         fields = ("id", "name", "phone", "address", "logo")
